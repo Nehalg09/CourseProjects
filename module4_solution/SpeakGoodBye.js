5501d999e96ca129175cb4045eba901df5c3ab1b
Nehal Goyal <goyalnehal5909@gmail.com>
2021-01-30 21:35:58 +0530

    module4 solution

---
+++ module4_solution/SpeakGoodBye.js
@@ -0,0 +1,15 @@
+
+var speakWord = "Good Bye";
+
+(function(window){
+  var byeSpeaker ={};
+  byeSpeaker.speakWord="Good Bye";
+  byeSpeaker.speak=function(name){
+    console.log(byeSpeaker.speakWord + " " +name);
+  }
+
+  window.byeSpeaker=byeSpeaker;
+
+})(window);
+
+
